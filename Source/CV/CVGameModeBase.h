15483e2bbdb2e1b423e38f114b7174742f62960e
Ignolek <ignole@icloud.com>
2022-12-13 18:03:54 +0100

    CV 0.8

---
+++ Source/CV/CVGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CVGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CV_API ACVGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
